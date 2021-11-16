5b4aae90837def0cc29ab2a2c10a1b9ce28bfb8d
Jackson R Wippel <jrwippel@hotmail.com>
2021-11-16 10:44:05 -0300

    Projeto Matriz

---
+++ src/main/java/Programa.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Programa {
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter values for row[M] and columns[N]:");
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] mat = new int[m][n];
+        for (int i = 0; i < mat.length;i++){
+            for (int j = 0; j < mat[i].length;j++){
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Enter an array value: ");
+        int number = sc.nextInt();
+
+        for (int i = 0;i < mat.length;++i){
+            for (int j = 0;j < mat[i].length;j++){
+                if (mat[i][j] == number) {
+                    System.out.println("Position:" + i+","+j);
+                    if (i > 0) {
+                        System.out.println("Left:" + mat[i][j - 1]);
+                    }
+                    if (j > 0) {
+                        System.out.println("UP:" + mat[i - 1][j]);
+                    }
+                    if (j < mat[i].length-1) {
+                        System.out.println("Right:" + mat[i][j + 1]);
+                    }
+                    if (i < mat.length-1) {
+                        System.out.println("Down:" + mat[i + 1][j]);
+                    }
+                }
+            }
+        }
+
+
+        sc.close();
+
+    }
+}
